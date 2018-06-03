c62e92575c9eff60f70aaebf235d1b98004ed79c
rishav-pandey <27219302+rishav-pandey@users.noreply.github.com>
2018-06-03 11:33:03 +0530

    added new button and basic functionaity

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  itemCount: number;
+  addBtnText: string = "Add a Task";
+  saveBtnText: string = "Save Your's Task";
+
+  todoItem: string;
+  todoList = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.itemCount = this.todoList.length;
+  }
+
+  addItem () {
+    this.todoList.push(this.todoItem);
+    this.todoItem = '';
+    this.itemCount = this.todoList.length;
   }
 
 }
